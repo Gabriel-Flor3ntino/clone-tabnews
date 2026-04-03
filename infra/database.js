ba1b72d9dcf8804f731c98c00ac36e079647849c
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-04-03 19:00:38 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const resultado = await client.query(queryObject);
-  await client.end();
-  return resultado;
-}
 
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
 export default {
   query: query,
 };
